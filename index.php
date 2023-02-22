77cf8249124b0f83a979c0adfc78ee315fed7f57
Robinwil <44304461+Robinwil@users.noreply.github.com>
2023-02-22 13:18:04 +0100

    Create index.php
    basic

---
+++ index.php
@@ -0,0 +1,21 @@
+!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta charset="UTF-8">
+  <title>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</title>
+  <link href="./css/style.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="css/style9.css">
+
+</head>
+
+  <body>
+  
+    <h1>hello world</h1>
+    
+  </body>
+  
+ <html>
